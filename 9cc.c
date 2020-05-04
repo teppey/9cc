3302ff330e4e1633f1576a1199981d1a70f8db61
Teppei Hamada <temada@gmail.com>
2020-05-04 16:38:21 +0900

    抽象構文木とそれを使ったコードジェネレータを導入

--- 9cc.c
+++ 9cc.c
@@ -25,6 +25,23 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs;     // 左辺
+    Node *rhs;     // 右辺
+    int val;       // kindがND_NUMの場合のみ使う
+};
+
 // 入力プログラム
 char *user_input;
 
@@ -123,9 +140,68 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr(void);
+Node *primary(void);
+
+Node *expr() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, primary());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary() {
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません\n");
+        error("引数の個数が正しくありません");
         return 1;
     }
 
@@ -135,27 +211,20 @@ int main(int argc, char **argv) {
     // トークナイズする
     token = tokenize(argv[1]);
 
+    // パースする
+    Node *node = expr();
+
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので、それをチェックして
-    // 最初のmov命令を出力
-    printf("  mov rax, %d\n", expect_number());
+    // 抽象構文木を下りながらコード生成
+    gen(node);
 
-    // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつアセンブリを
-    // 出力
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
-
+    // スタックトップに式全体の値が残っているはずなので
+    // それをRAXにロードしてから関数の返り値とする
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
